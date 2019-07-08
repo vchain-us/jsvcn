a4e9523156c9ef02dc05273825a1452a25db24fa
Peter Varga <admin@vargapeter.com>
2019-07-08 11:22:04 +0200

    enable http

--- utils/misc.js
+++ utils/misc.js
@@ -1,3 +1,3 @@
-const isValidLocalPath = (file) => typeof file === "string" && file.lastIndexOf("file://", 0) === 0
+const isValidLocalPath = (file) => typeof file === "string" && (file.lastIndexOf("file://", 0) === 0 || file.lastIndexOf("http://", 0) === 0 || file.lastIndexOf("https://", 0) === 0)
 
 export { isValidLocalPath }
