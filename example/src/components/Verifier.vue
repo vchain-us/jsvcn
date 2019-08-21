dd4b39e4e8f616846efc03ed167f733620521503
Peter Varga <admin@vargapeter.com>
2019-08-21 17:03:12 +0200

    feat: extend verifier example

--- example/src/components/Verifier.vue
+++ example/src/components/Verifier.vue
@@ -1,26 +1,43 @@
 <template>
   <div>
     <h1>Jsvcn Demo</h1>
-    <h3>Select a file</h3>
-    <input type="file" id="file" name="file" @change="onFileChange" />
 
-    <h3>Vertification against the Blockchain:</h3>
-    <p>Status: {{asset.status}}</p>
-    <p v-if="progress!==0">Progress {{progress}}</p>
-    <p>
-      Raw response:
-      <br />
-      <textarea rows="10">{{asset}}</textarea>
-    </p>
+    <fieldset>
+      <legend>Verify a file</legend>
+      <input type="file" id="file" name="file" @change="onFileChange" />
+    </fieldset>
+    <br />
+    <fieldset>
+      <legend>Verify a hash</legend>
+      <input type="text" v-model="hash" @change="onHashChange" />
+    </fieldset>
 
-    <h3>Vertification against Organization "vchain.us":</h3>
-    <p>Status: {{orgAsset.status}}</p>
-    <p v-if="orgProgress!==0">Progress {{orgProgress}}</p>
-    <p>
-      Raw response:
-      <br />
-      <textarea rows="10">{{orgAsset}}</textarea>
-    </p>
+    <h1>Results</h1>
+
+    <div>Progress: {{progress}}</div>
+    <div>Org. Progress: {{orgProgress}}</div>
+
+    <fieldset v-if="asset">
+      <legend>Vertification:</legend>
+      <p>Status: {{asset.status}}</p>
+      <p v-if="progress!==0">Progress {{progress}}</p>
+      <p>
+        Raw response:
+        <br />
+        <textarea rows="10">{{asset}}</textarea>
+      </p>
+    </fieldset>
+
+    <fieldset v-if="orgAsset">
+      <legend>Vertification against Organization "vchain.us":</legend>
+      <p>Status: {{orgAsset.status}}</p>
+      <p v-if="orgProgress!==0">Progress {{orgProgress}}</p>
+      <p>
+        Raw response:
+        <br />
+        <textarea rows="10">{{orgAsset}}</textarea>
+      </p>
+    </fieldset>
   </div>
 </template>
 
@@ -32,26 +49,36 @@ export default {
     msg: String
   },
   data: () => ({
-    asset: {},
-    orgAsset: {},
+    asset: null,
+    hash: null,
+    orgAsset: null,
     progress: 0,
     orgProgress: 0
   }),
   methods: {
+    async verify(target) {
+      const jsvcn = new Jsvcn();
+      const result = await jsvcn.verify(target, this.onProgressChange);
+      console.log(result)
+      this.asset = result;
+    },
+    async verifyAgainstOrg(target) {
+      const jsvcnOrg = new Jsvcn();
+      const result = await jsvcnOrg.verify(
+        target,
+        this.onProgressChange,
+        "summitsport.hu"
+      );
+      this.orgAsset = result;
+    },
     async onFileChange(event) {
       const file = event.target.files[0];
-
-      const jsvcn = new Jsvcn();
-      const { asset } = await jsvcn.verify(file, this.onProgressChange);
-      this.asset = asset;
-
-      const jsvcnOrg = new Jsvcn();
-      const { asset: orgAsset } = await jsvcn.verify(
-        file,
-        this.onProgressChange,
-        "vchain.us"
-      );
-      this.orgAsset = orgAsset;
+      await this.verify(file);
+      await this.verifyAgainstOrg(file);
+    },
+    async onHashChange() {
+      await this.verify(this.hash);
+      await this.verifyAgainstOrg(this.hash);
     },
     onProgressChange(progress) {
       this.progress = progress; // returns only when file size is 50Mb+..
