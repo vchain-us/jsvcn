063adaa31035d320a80ba77efb8d87600ccb9575
Peter Varga <admin@vargapeter.com>
2019-08-30 07:50:49 +0200

    chore: rollup - include helpers to bundle, example updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,29 @@
 import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
 import minify from 'rollup-plugin-babel-minify';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
 	input: './index.js',
 	output: {
 		file: './dist/jsvcn.min.js',
-		name: 'jsvcn',
+		name: 'Jsvcn',
 		format: 'iife',
 		compact: true,
 		external: ['ethers']
 	},
 	plugins: [
 		babel({
-			runtimeHelpers: true
+			runtimeHelpers: true,
+			externalHelpers: true,
+		}),
+		nodeResolve({
+			jsnext: true,
+			main: true
+		}),
+		commonjs({
+
 		}),
 		json({
 			compact: true
