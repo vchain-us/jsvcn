2ea3d806448acc745dd915834f33400c73fcf20d
Peter Varga <admin@vargapeter.com>
2019-08-30 07:00:42 +0200

    chore: os dep. removed, rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
 
 export default {
-	input: './cli.js',
+	input: './index.js',
 	output: {
-		file: './dist/cli.js',
+		file: './dist/jsvcn.js',
 		name: 'jsvcn',
-		format: 'umd',
+		format: 'iife',
 		compact: true
 	},
 	plugins: [
